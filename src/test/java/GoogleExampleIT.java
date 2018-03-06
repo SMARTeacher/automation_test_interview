54581fece6d1e2fbd5e415def63608e4434127a1
emilg7 <37091837+emilg7@users.noreply.github.com>
2018-03-06 18:04:45 -0500

    GoogleExampleIT - updated tests

--- src/test/java/GoogleExampleIT.java
+++ src/test/java/GoogleExampleIT.java
@@ -9,41 +9,42 @@
  * */
 
 
-@Test(groups={"localTest"})
-public class GoogleExampleIT {
-	private static final Logger LOGGER = LogManager.getLogger(GoogleExampleIT.class.getName());
+public class GoogleExampleIT 
+	{
 	private WebDriver driver;
-	@FindBy(css = "[name='q']")
-	private WebElement searchBar;
-
-	@Test
-	public void googleCheeseExample() throws Exception {
-		searchBar.clear();
-		searchBar.sendKeys("Cheese!");
-		searchBar.sendKeys(Keys.ENTER);
-		LOGGER.info("Page title is: " + driver.getTitle());
-		Assert.assertTrue(driver.getTitle().equals("Google"));
-	}
-
-	@Test(enabled = true)
-	public void googleMilkExample() throws Exception {
-		searchBar.clear();
-		searchBar.sendKeys("Milk!");
-		searchBar.sendKeys(Keys.ENTER);
-		LOGGER.info("Page title is: {}", driver.getTitle());
-		Assert.assertTrue(driver.getTitle().equals("Google"));
-	}
-
+	
 	@BeforeMethod
-	public void setUp() {
-		
-		driver = new new FirefoxDriver();
-		PageFactory.initElements(new AjaxElementLocatorFactory(driver, 60), this);
-		driver.get("http://www.google.com");
+	public void setUp() throws Exception
+	{
+		driver = new ChromeDriver();
+		driver.get("https://www.google.ca/");
 	}
 
 	@AfterMethod
-	public void tearDown() {
-		driver.close();
+	public void tearDown() throws Exception
+
+	{
+		driver.quit();
 	}
+
+	@Test (enabled = true)
+	public void googleCheeseExample() throws Exception
+
+	{
+		WebElement searchBar = driver.findElement(By.name("q"));
+		searchBar.sendKeys("Cheese!");
+		searchBar.submit();
+		Assert.assertEquals(driver.getTitle(), "Cheese! - Google Search");
+	}
+	
+	@Test (enabled = true)
+	public void googleMilkExample() throws Exception
+
+	{
+		WebElement searchBar = driver.findElement(By.name("q"));
+		searchBar.sendKeys("Milk!");
+		searchBar.submit();
+		Assert.assertEquals(driver.getTitle(), "Milk! - Google Search");
+	}
+
 }
